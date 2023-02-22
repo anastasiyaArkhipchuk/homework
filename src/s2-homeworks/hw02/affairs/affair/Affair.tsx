26c43bff3e9ae25ab6ca4328a6c20a504c4bb40c
nastyashvec42 <nastyashvec42@gmail.com>
2023-02-22 20:23:05 +0300

    Second homeworkh

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -25,7 +25,7 @@ function Affair(props: AffairPropsType) {
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-hhhhh
+hhhhhhjj
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>

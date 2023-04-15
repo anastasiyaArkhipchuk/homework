12c64239192810051252990b0114764ca4e2c4f0
anastasiyaArkhipchuk <nastyashvec42@gmail.com>
2023-04-15 17:07:03 +0300

    hw3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -76,7 +76,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = 'jkff' // need to fix
+    const lastUserName = 'jkffk' // need to fix
 
     return (
         <Greeting
